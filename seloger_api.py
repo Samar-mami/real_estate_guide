e9b7b0cba0c25ddbbae544085003a359e634a513
samar.mami18@gmail.com <samar.mami18@gmail.com>
2023-10-10 15:05:38 +0200

    site Zalando

--- seloger_api.py
+++ seloger_api.py
@@ -1,16 +1,26 @@
 import requests
-import pandas as pd
+from bs4 import BeautifulSoup
 
-url = "https://seloger.p.rapidapi.com/locations/search"
-
-querystring = {"searchTerm": "paris"}
-
+url = 'https://zalando.fr/'
 headers = {
-    "X-RapidAPI-Key": "4462bef6d9mshffa2af5b3dd7219p193e1cjsn625dfb1b6d9c",
-    "X-RapidAPI-Host": "seloger.p.rapidapi.com"
-}
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/58.0.3029.110 Safari/537.3'}
+cookies = {'cookie_name': 'cookie_value'}
+with requests.Session() as session:
+    page = session.get(url, headers=headers, cookies=cookies,verify=False)
 
-response = requests.get(url, headers=headers, params=querystring, verify=False)
-response_json = response.json()
-df = pd.DataFrame(response_json)
-print(df.head())
+if page.status_code == 200:
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Now you can use BeautifulSoup to navigate and extract information from the HTML
+    # For example, let's print the title of the page
+    title = soup.title
+    contents = soup.contents
+    print(f'Title: {title.text}')
+    print(f'Body: {contents.}')
+
+    # You can explore the HTML structure of the page and extract other information as needed
+
+else:
+    print(f'Failed to retrieve the page. Status code: {page.status_code}')
